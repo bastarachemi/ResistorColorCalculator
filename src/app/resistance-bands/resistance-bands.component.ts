9ac679c9072476fa671492de55a0cb26a6026e1f
Michael Bastarache <mb00147@my.westga.edu>
2021-07-21 18:51:02 -0400

    Created resistant-bands component with four methods

--- src/app/resistance-bands/resistance-bands.component.ts
+++ src/app/resistance-bands/resistance-bands.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BandsService } from '../services/bands.service';
 
 @Component({
   selector: 'app-resistance-bands',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResistanceBandsComponent implements OnInit {
 
-  constructor() { }
+  public bands: string[] = ['','','','',''];
+  public digitColors: string[];
+  public multiplierColors: string[];
+  public toleranceColors: string[];
+  public resistance: string = "";
 
-  ngOnInit(): void {
+  constructor(private bandsService: BandsService) {
+    this.digitColors = ['black','brown','red','orange','yellow','green','blue','violet','grey','white'];
+    this.multiplierColors = ['black','brown','red','orange','yellow','green','blue','violet','grey','white','gold','silver'];
+    this.toleranceColors = ['brown','red','green','blue','violet','grey','gold','silver','none'];
+  }
+
+  ngOnInit() {
+
+  }
+
+  onChange(value: any, index: any) {
+    this.bands[index] = value;
+    this.bandsService.addBand(value,index);
+    if (!this.hasEmptyBands()) {
+      this.resistance = this.bandsService.getCalculatedResistance();
+    }
+  }
+
+  private hasEmptyBands(): boolean {
+    for (let band of this.bands) {
+      if (band == "") {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  clear() {
+    this.bands = ['','','','',''];
+    this.bandsService.clear();
+    this.resistance = "";
+  }
+
+  isBgDark(color: string) {
+    let darkColors = ["black","brown","blue","green","grey"];
+    if (darkColors.includes(color)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
