5f8d3d23abc215dbf73069e8c4a8e70865c6a1b8
Michael Bastarache <mb00147@my.westga.edu>
2021-07-21 18:30:54 -0400

    Added private helper methods to calculate resistance in service

--- src/app/services/bands.service.ts
+++ src/app/services/bands.service.ts
@@ -39,6 +39,33 @@ export class BandsService {
     this.bands[index] = color;
   }
 
+  getCalculatedResistance() {
+    if (!this.hasEmptyBands()) {
+      this.resistance = this.getDigits() * this.getMultiplier()[0] + this.getMultiplier()[1];
+      this.resistance += " +/- " + this.getTolerance() + "%";
+      return this.resistance;
+    } else {
+        return "";
+    }
+  }
+
+  private getDigits() {
+    let firstDigit = this.colorData.find(firstBand => firstBand.color == this.bands[0]).digit;
+    let secondDigit = this.colorData.find(secondBand => secondBand.color == this.bands[1]).digit;
+    let thirdDigit = this.colorData.find(thirdBand => thirdBand.color == this.bands[2]).digit;
+    return parseInt(firstDigit + secondDigit + thirdDigit);
+  }
+
+  private getMultiplier() {
+    let multiplier = this.colorData.find(fourthband => fourthband.color == this.bands[3]).multiplier;
+    return multiplier;
+  }
+
+  private getTolerance() {
+    let tolerance = this.colorData.find(fifthBand => fifthBand.color == this.bands[4]).tolerance;
+    return tolerance;
+  }
+
   clear() {
     this.bands = ['','','','',''];
     this.resistance = "";
