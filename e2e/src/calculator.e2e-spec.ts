289da1c2a139640a93f31367cbcc877f0975fedc
Michael Bastarache <mb00147@my.westga.edu>
2021-07-21 20:07:59 -0400

    Added two more e2e tests to Calculator

--- e2e/src/calculator.e2e-spec.ts
+++ e2e/src/calculator.e2e-spec.ts
@@ -44,5 +44,32 @@ describe('Calculator Page', function() {
     expect(element(by.id("resistanceMessage")).getText()).toEqual("Resistance: 865M +/- 0.1%");
   });
 
+  it('should display correct resistance if band colors change', function() {
+    calculatorPage.selectBandColor("grey",0);
+    calculatorPage.selectBandColor("blue",1);
+    calculatorPage.selectBandColor("green",2);
+    calculatorPage.selectBandColor("blue",3);
+    calculatorPage.selectBandColor("violet",4);
+    expect(element(by.id("resistanceMessage")).getText()).toEqual("Resistance: 865M +/- 0.1%");
+    calculatorPage.selectBandColor("orange",3);
+    expect(element(by.id("resistanceMessage")).getText()).toEqual("Resistance: 865K +/- 0.1%");
+  });
+
+  it('should clear all bands if clear button clicked', function() {
+    calculatorPage.selectBandColor("grey",0);
+    expect(element(by.id("band0")).element(by.css('option:checked')).getText()).toEqual("grey");
+    calculatorPage.selectBandColor("blue",1);
+    calculatorPage.selectBandColor("green",2);
+    calculatorPage.selectBandColor("blue",3);
+    calculatorPage.selectBandColor("violet",4);
+    expect(element(by.id("resistanceMessage")).getText()).toEqual("Resistance: 865M +/- 0.1%");
+    calculatorPage.clearButton();
+    expect(element(by.id("resistanceMessage")).getText()).toEqual("Resistance:");
+    expect(element(by.id("band0")).element(by.css('option:checked')).isPresent()).toBeFalsy();
+    expect(element(by.id("band1")).element(by.css('option:checked')).isPresent()).toBeFalsy();
+    expect(element(by.id("band2")).element(by.css('option:checked')).isPresent()).toBeFalsy();
+    expect(element(by.id("band3")).element(by.css('option:checked')).isPresent()).toBeFalsy();
+    expect(element(by.id("band4")).element(by.css('option:checked')).isPresent()).toBeFalsy();
+  });
 
   });
